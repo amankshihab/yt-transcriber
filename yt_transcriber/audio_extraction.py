2fd2e25da42a199b4f9d86ac18bb18b4ec846fdf
Krishna Bhat <61185596+krishnabhat3383@users.noreply.github.com>
2024-04-05 19:30:18 +0530

    Added `pathlib` to handle paths for files and directories for additional cross-platform benefits. (#2)
    * Added properties for audio_folder and model_path
    
    * Added import for pathlib
    
    * Added typehints to include Path
    
    * Added a fix to make sure to convert Path to str
    
    * Added imports for pathlib and typing (for typehints)
    
    * Fixed documentation
    
    * Fixed the issue of assert being used where raise must be.
    
    * Changed str to Path
    
    * Fixed documentation, used pathlib feature to return Path instead of
    str
    
    * Fixed path to str bug
    
    * Added docstring to the Transcribe class, made audio_extractor private
    and fixed format issues

--- yt_transcriber/audio_extraction.py
+++ yt_transcriber/audio_extraction.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from typing import Optional
+
 from pytube import YouTube
 
 
@@ -6,15 +9,17 @@ class AudioExtractor:
 
     Attributes
     ----------
-    parent_folder : str
+    parent_folder : str | Path
         Path to the parent folder that contains the extracted audio files.
     """
 
-    def __init__(self, parent_folder: str):
-        assert parent_folder is not None, "Please provide a path to parent folder."
-        self.parent_folder = parent_folder
+    def __init__(self, parent_folder: str | Path):
+        if parent_folder is None:
+            raise ValueError("Please provide a path to parent folder.")
 
-    def download_audio_from_yt(self, url: str, filename: str | None = None) -> str:
+        self.parent_folder = Path(parent_folder)
+
+    def download_audio_from_yt(self, url: str, filename: Optional[str] = None) -> Path:
         """Downloads the audio from youtube videos to the directory specified.
         Optionally, the filename can be changed.
 
@@ -23,11 +28,11 @@ class AudioExtractor:
         url : str
             The url to the youtube video of which the audio is to be downloaded.
 
-        filename : str, optional
+        filename : str | Path, optional
             The filename for the audio file to be downloaded.
         """
         yt = YouTube(url)
         stream = yt.streams.filter(only_audio=True)
         _filename = filename if filename is not None else f"{yt._title} audio"
         stream[0].download(output_path=self.parent_folder, filename=_filename + ".mp3")
-        return self.parent_folder + '/' + _filename + ".mp3"
+        return self.parent_folder / f"{_filename}.mp3"
--- yt_transcriber/transcribe.py
+++ yt_transcriber/transcribe.py
@@ -1,19 +1,48 @@
-from .audio_extraction import AudioExtractor
+from pathlib import Path
 
 from faster_whisper import WhisperModel
 
+from .audio_extraction import AudioExtractor
+
 
 class Transcriber:
+    """Class that encapsulates the transcription functionality
+
+    Attributes
+    ----------
+    audio_folder: str | Path
+        Path to where the audio files need to be stored
+    model_path: str | Path
+        Path to where the WhisperModel binary for use in transcription.
+    device: str
+        The hardware device name to be used while transcribing the video.
+    """
     def __init__(
         self,
-        audio_folder: str,
-        model_path: str,
+        audio_folder: str | Path,
+        model_path: str | Path,
         device: str,
     ):
         self.audio_folder = audio_folder
-        self.audio_extractor = AudioExtractor(self.audio_folder)
+        self._audio_extractor = AudioExtractor(self.audio_folder)
         self.model_path = model_path
         self.device = device
+
+    @property
+    def audio_folder(self):
+        return str(self._audio_folder)
+
+    @audio_folder.setter
+    def audio_folder(self, folder_loc: str | Path):
+        self._audio_folder = Path(folder_loc).absolute()
+
+    @property
+    def model_path(self):
+        return str(self._model_path)
+
+    @model_path.setter
+    def model_path(self, path: str | Path):
+        self._model_path = Path(path).absolute()
 
     def transcribe(self, urls: str | list[str]) -> list[str]:
         """
@@ -37,7 +66,7 @@ class Transcriber:
         for url in _urls:
             transcript = ""
             print(f"Fetching audio for {url}..")
-            audio_file = self.audio_extractor.download_audio_from_yt(url)
+            audio_file = str(self._audio_extractor.download_audio_from_yt(url).absolute())
             print(f"Transcribing {audio_file}")
             segments, _ = model.transcribe(audio_file)
             print("Transcription complete.")
